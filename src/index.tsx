2fb146205365c6bf25aefad35582bcca5c732607
Simon Dell <simon.dell@founders4schools.org.uk>
2020-11-24 11:07:20 +0000

    Remove redundant import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Consumer from "./Consumer";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
